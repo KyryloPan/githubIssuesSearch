54c210086572a756552b2594d905dcdd0064c0f5
Kyrylo Panov <kyrylo.panov@visma.com>
2021-09-06 16:35:44 +0300

    Updated rootReducer

--- redux/index.ts
+++ redux/index.ts
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { githubIssuesReducer } from './reducers/githubIssuesReducer';
+import { githubIssuesReducer, GlobalIssuesState } from './reducers/githubIssuesReducer';
 
+export interface GlobalStateFull {
+  githubIssues: GlobalIssuesState;
+}
 
-const rootReducer = combineReducers({ githubIssuesReducer });
+const rootReducer = combineReducers<GlobalStateFull>({
+  githubIssues: githubIssuesReducer
+});
 
 export type ApplicationState = ReturnType<typeof rootReducer>;
 const store = createStore(rootReducer, applyMiddleware(thunk));
