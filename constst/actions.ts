09dcfd5d4b4823186be4877d350eae475cd91517
Kyrylo Panov <kyrylo.panov@visma.com>
2021-09-06 16:32:00 +0300

    Minor update to consts and actions variables

---
+++ constst/actions.ts
@@ -0,0 +1,7 @@
+export const ACTION_GITHUB_GET_ISSUES_PENDING: string = 'github_get_issues_pending';
+export const ACTIONS_GITHUB_ISSUES_LOAD: string = 'github_issues_load';
+export const ACTIONS_GITHUB_CLEAR_ISSUES: string = 'github_clear_issues';
+export const ACTIONS_GITHUB_GET_ISSUES_ERROR: string = 'github_get_issues_error';
+export const ACTIONS_GITHUB_SET_ORGANIZATION_ID: string = 'github_set_organisation_id';
+export const ACTIONS_GITHUB_SET_REPO_ID: string = 'github_set_repo_id';
+export const ACTIONS_GITHUB_SET_PAGE: string = 'github_set_page';
---
+++ constst/constants.ts
@@ -0,0 +1,5 @@
+// Load statuses
+export const LOAD_STATUS_INIT = 'init';
+export const LOAD_STATUS_LOADING = 'loading';
+export const LOAD_STATUS_LOADED = 'loaded';
+export const LOAD_STATUS_ERROR = 'error';
